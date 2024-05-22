1f453e81df0579564dec73d01eebecf27eceff3e
Niko <nikolasm@outlook.de>
2024-05-22 09:44:26 +0200

    added first value components

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+const Value = ({ label, value }) => (
+  <View>
+    <Text style={styles.label}>{label}</Text>
+    <Text style={styles.value}>{value}</Text>
+  </View>
+);
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello!</Text>
+
+      <View style={styles.values}>
+        <Value label="Steps" value="1219" />
+        <Value label="Distance" value="0,75 km" />
+        <Value label="Flights Climbed" value="12" />
+      </View>
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +29,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'black',
     justifyContent: 'center',
+    padding: 12,
+  },
+  values: {
+    flexDirection: 'row',
+    gap: 25,
+    flexWrap: 'wrap',
+  },
+  label: {
+    color: 'white',
+    fontSize: 20,
+  },
+  value: {
+    fontSize: 40,
+    color: '#AFB3BE',
+    fontWeight: '500',
   },
 });
