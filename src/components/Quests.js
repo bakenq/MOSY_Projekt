8d69b2492237ac2211415ff18ac1c6ad2cf22eb3
Niko <nikolasm@outlook.de>
2024-06-15 14:36:33 +0200

    Added daily reset of quests

--- src/components/Quests.js
+++ src/components/Quests.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, ScrollView, Text, TouchableOpacity } from 'react-native';
+import { useIsFocused } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
@@ -12,7 +13,10 @@ const Quests = ({ steps }) => {
   const [completedQuests, setCompletedQuests] = useState([]);
   const [claimedQuests, setClaimedQuests] = useState([]);
 
+  const isFocused = useIsFocused();
+
   // Function to clear all data from AsyncStorage
+  // (Unused right now, but useful for debugging)
   const clearAllAsyncStorage = async () => {
     try {
       await AsyncStorage.clear();
@@ -24,9 +28,14 @@ const Quests = ({ steps }) => {
 
   useEffect(() => {
     // Load user data
-    setCompletedQuests([]);
     loadData();
   }, []);
+
+  useEffect(() => {
+    if (isFocused) {
+      resetClaimedQuestsDaily();
+    }
+  }, [isFocused]);
 
   useEffect(() => {
     // Check for quest completion
@@ -34,12 +43,15 @@ const Quests = ({ steps }) => {
   }, [steps, completedQuests]);
 
   useEffect(() => {
+    // Debugging Logs
     console.log('Updated User XP:', userXP);
     console.log('Updated User Level:', userLevel);
     console.log('Updated Completed Quests:', completedQuests);
     console.log('Updated Claimed Quests:', claimedQuests);
+
     saveData();
   }, [userXP, userLevel, completedQuests, claimedQuests]); // userXP would probably be enough?
+
 
   const loadData = async () => {
     const xp = await AsyncStorage.getItem('userXP');
@@ -56,6 +68,7 @@ const Quests = ({ steps }) => {
   };
 
   const saveData = async () => {
+    const reset = []; // For debugging
     try {
       await AsyncStorage.setItem('userXP', userXP.toString());
       await AsyncStorage.setItem('userLevel', userLevel.toString());
@@ -78,13 +91,27 @@ const Quests = ({ steps }) => {
     });
   };
 
+  const isQuestClaimableToday = async (questId) => {
+    const today = new Date().toISOString().split('T')[0];
+    const questClaimedToday = claimedQuests.some(claim => claim.questId === questId && claim.dateClaimed === today);
+    return !questClaimedToday && completedQuests.includes(questId);
+  };
+
+  function resetClaimedQuestsDaily() {
+    const today = new Date().toISOString().split('T')[0];
+    const questsClaimedToday = claimedQuests.filter(claim => claim.dateClaimed === today);
+    setClaimedQuests(questsClaimedToday);
+  };
+
+
   const claimReward = async (questId) => {
     const quest = quests.find(q => q.id === questId);
-    if (claimedQuests.includes(questId)) {
+    // if (claimedQuests.some(claim => claim.questId === questId)) {
+    if (!isQuestClaimableToday(questId)) {
       console.log(`Quest ${questId} already claimed!`);
 
     } else if (quest && completedQuests.includes(questId)) {
-      
+
 
       let newXP = userXP + quest.xpReward;
       let newLevel = userLevel;
@@ -95,19 +122,23 @@ const Quests = ({ steps }) => {
         newXP -= 1000;
       }
 
+      const today = new Date().toISOString().split('T')[0];
+      const newClaim = { questId, dateClaimed: today };
+
       console.log(`Claimed ${quest.xpReward} XP from quest ${questId}`);
 
       // Ensure state updates are applied before saving
       await new Promise(resolve => setTimeout(resolve, 0)); // Wait for next event loop tick
 
-      // Increment number of completed quests
-      setNumberOfCompletedQuests(numberOfCompletedQuests + 1);
-      // Add quest to claimed quests
-      setClaimedQuests([...claimedQuests, questId]);
-
       // Update state once with the new values
       setUserLevel(newLevel);
       setUserXP(newXP);
+
+      // Increment number of completed quests
+      setNumberOfCompletedQuests(numberOfCompletedQuests + 1);
+      // Add quest to claimed quests with date
+      setClaimedQuests([...claimedQuests, newClaim]);
+
 
       // Debugging: Log the updated value from AsyncStorage
       AsyncStorage.getItem('userXP').then(storedXP => {
@@ -122,18 +153,18 @@ const Quests = ({ steps }) => {
         <View key={quest.id} style={styles.questItem}>
           <Text style={styles.questDescription}>{quest.description}</Text>
           <Text style={styles.questExp}>Reward: {quest.xpReward} XP</Text>
-          {completedQuests.includes(quest.id) && !claimedQuests.includes(quest.id) ? (
+          {completedQuests.includes(quest.id) && !claimedQuests.some(claim => claim.questId === quest.id) ? (
             <TouchableOpacity style={styles.button} onPress={() => claimReward(quest.id)} >
               <Text style={styles.buttonText}>Claim Reward</Text>
             </TouchableOpacity>
-          ) : claimedQuests.includes(quest.id) ? (
+          ) : claimedQuests.some(claim => claim.questId === quest.id) ? (
             <TouchableOpacity style={[styles.button, styles.buttonClaimed]} disabled={true} >
               <Text style={styles.buttonText}>Reward Claimed</Text>
             </TouchableOpacity>
           ) : (
-          <TouchableOpacity style={[styles.button, styles.buttonDisabled]} disabled={true} >
-            <Text style={styles.buttonText}>Quest Incomplete</Text>
-          </TouchableOpacity>
+            <TouchableOpacity style={[styles.button, styles.buttonDisabled]} disabled={true} >
+              <Text style={styles.buttonText}>Quest Incomplete</Text>
+            </TouchableOpacity>
           )}
         </View>
       ))}
